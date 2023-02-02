15694b6b3fb2659f043b4fa785729205ca81fda7
R-SMP <raul.s.miguel@alumnos.uc3m.es>
2023-02-02 23:02:30 +0100

    fixed global pMOR
    Barring any mistakes, global pMOR is now correctly implemented.

--- beam_mor_pmor.m
+++ beam_mor_pmor.m
@@ -118,16 +118,16 @@ xlabel('Frequency (rad/s)')
 % fprintf("Error norm for IRKA: %.3e\n", ...
 %     error_norm_irka);
 
-%% Wth
-
-tol = 1e-10;
-maxiter = 10;
-s0 = 1i*linspace(0,10000,20);
-
-[M, D, K, f, C] = fem_beam(1,10);
-
-
-[Kr, Gr, Mr, Br, Cr, s, Vr] = SO_IRKA_SISO(K, D, M, f, C, s0, 'os', tol, maxiter)
+% %% Wth
+% 
+% tol = 1e-10;
+% maxiter = 10;
+% s0 = 1i*linspace(0,10000,20);
+% 
+% [M, D, K, f, C] = fem_beam(1,10);
+% 
+% 
+% [Kr, Gr, Mr, Br, Cr, s, Vr] = SO_IRKA_SISO(K, D, M, f, C, s0, 'os', tol, maxiter);
 
 
 %% Global pMOR
@@ -175,9 +175,9 @@ for i = 1:size(S,1)
 
 end
 
-S(S<1e-10) = 0;
-
-V = U*S*Vsd';
+% S(S<1e-10) = 0;
+% 
+% V = U*S*Vsd';
 
 %Final global basis %Not sure how I should be gettin it from here
 
@@ -203,19 +203,18 @@ s = 1i*logspace(0,4,100); %Some trouble somewhere
 
 result_global = zeros(1,length(s));
 for ii=1:length(s)
-    result_irka(ii) = Cr * ((s(ii)^2*Mr + s(ii)*Dr + Kr) \ fr);
+    result_global(ii) = Cr * ((s(ii)^2*Mr + s(ii)*Dr + Kr) \ fr);
     %Careful, s should be the initial frequency range, not the eigenvalues
     %obtained from IRKA
 end
 
 %Error calculations
-error_irka = (result_full - result_global)./result_full;
-error_norm_irka = norm(result_full - result_global)/norm(result_full);
+% error_irka = (result_full - result_global)./result_full;
+% error_norm_irka = norm(result_full - result_global)/norm(result_full);
 
 %Plot reponse using GLOBAL BASIS
 fig = figure('Name','Frequency response using Global MOR');
 set(fig,'defaulttextinterpreter','latex')
-hold on
 semilogy(abs(s),abs(result_global))
 xlim([0 10000])
 ylim([1e-8 1e-1])
@@ -223,20 +222,20 @@ title('Global MOR')
 ylabel('Displacement at load point')
 xlabel('Frequency (rad/s)')
 
-% %Plot reponse using GLOBAL BASIS
-% fig = figure('Name','Frequency response using IRKA');
-% set(fig,'defaulttextinterpreter','latex')
-% semilogy(abs(s),abs(result_full))
-% hold on
-% semilogy(abs(s),abs(result_irka))
-% hold on
-% semilogy(abs(s),abs(result_global))
-% xlim([0 10000])
-% ylim([1e-8 1e-1])
-% legend('Full', 'SO-IRKA', 'Global MOR')
-% title('Harmonic response of a clamped plate')
-% ylabel('Displacement at load point')
-% xlabel('Frequency (rad/s)')
+%Plot reponse using GLOBAL BASIS
+fig = figure('Name','Frequency response comparison');
+set(fig,'defaulttextinterpreter','latex')
+semilogy(abs(s),abs(result_full), 'LineWidth', 6)
+hold on
+semilogy(abs(s),abs(result_irka), 'LineWidth', 4)
+hold on
+semilogy(abs(s),abs(result_global), 'LineWidth', 2)
+xlim([0 10000])
+ylim([1e-8 1e-1])
+legend('Full', 'SO-IRKA', 'Global MOR')
+title('Harmonic response of a clamped plate')
+ylabel('Displacement at load point')
+xlabel('Frequency (rad/s)')
 
 
 
@@ -260,7 +259,7 @@ k = length(P);
 
 V = []; %Not sure if this is effective or if preallocating would be better
 
-%This matrices will store the reduced system matrices for each parameter
+%These matrices will store the reduced system matrices for each parameter
 %value
 Mp = [];
 Dp = [];
@@ -273,36 +272,56 @@ for i = 1:k
     %Create model matrices for that parameter
     [M, D, K, f, C] = fem_beam(P(i),10);
 
-    Mp = [M, Mr];
+    %Mp = [Mp, Mr];
 
 
-    %Obtain basis matrix for that parameter
-    [~, ~, ~, ~, ~, ~, Vr] = SO_IRKA_SISO(K, D, M, f, C, s0, 'os', tol, maxiter);
+    %Obtain basis matrix and reduced system matrices for that parameter
+    [Kr, Dr, Mr, fr, Cr, sr, Vr] = SO_IRKA_SISO(K, D, M, f, C, s0, 'os', tol, maxiter);
 
     %Concatenate
     V = [V, Vr];
 
-    Mp = [M, Mr];
-    Dp = [D, Dr];
-    Kp = [K, Kr];
-    fp = [f, fr];
-    Cp = [C, Cr];
+    Mp = [Mp, Mr];
+    Dp = [Dp, Dr];
+    Kp = [Kp, Kr];
+    fp = [fp, fr];
+    Cp = [Cp; Cr];
         
 end
 
 %Perform SVD to obtain r most significant basis vectors
 [U, S, Vsd] = svd(V);
 
-r = length(s0);
+r = 2*length(s0); %Remember the SO-IRKA algorithm doubles r
 
 R = U(:,1:r);
 
 %Transform reduced system matrices to generalized coordinate system R
 
-for i = 1:k
-    
-    T = R'*V(:,(1:r)) %Figure out proper index distribution
+Mgp = [];
+Dgp = [];
+Kgp = [];
+fgp = [];
+Cgp = [];
 
+for i = 1:k
+
+    Vrt  = V(:,((k-1)*r + 1):k*r);
+    
+    T = inv(R'*V(:,((k-1)*r + 1):k*r));
+
+    M_tilde_k = Mp(:,((k-1)*r + 1 ):k*r);
+    D_tilde_k = T'*Dp(:,((k-1)*r + 1):k*r)*T;
+    K_tilde_k = T'*Kp(:,((k-1)*r + 1):k*r)*T;
+    f_tilde_k = T'*fp(:,k);
+    C_tilde_k = Cp(k,:);
+
+    Mgp = [Mgp, M_tilde_k];
+    Dgp = [Dgp, D_tilde_k];
+    Kgp = [Kgp, K_tilde_k];
+    fgp = [fgp, f_tilde_k];
+    Cgp = [Cgp; C_tilde_k];
+% 
 end
 
 
